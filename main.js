95d193239107da2c8ac83da4c47fdbd7ecb04bc8
Alessandro Lombardi <alessandro.lombardi5@studio.unibo.it>
2019-01-27 11:23:41 +0100

    Simple movements in all directions

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ var width = 17;
 var height = 2;
 var stairsForFloor = 3;
 
+/*Environment*/
+var walkable = "walkable";
+/*Style*/
+var playerC = "player";
+var backgroundC = "background";
+var stairC = "stair";
+var floorC = "floor";
+
 function generateRandomPositionStair(){
 		var res = [];
 		var tmp = [];
@@ -17,61 +25,61 @@ function generateRandomPositionStair(){
 		return res;
 }
 
-function generateFloor(stair, holeRight) {
+function generateFloor(stairs, holeRight) {
 		var res = '<tr>';
 		var max = width;
 		var i = 0;
 		if(holeRight === false){
-				res+='<td class="air">0</td>';
+				res+='<td class="' + backgroundC + '">0</td>';
 				i++;
 		} else if(holeRight === true) {
 				max--;
 		}
 		for(i; i < max; i++) {
-				if(!stair.includes(i)) {
-					res+='<td class="floor">0</td>';
+				if(!stairs.includes(i)) {
+					res+='<td class="' + floorC + '">0</td>';
 				} else {
-					res+='<td class="stair">0</td>';
+					res+='<td class="' + stairC + ' ' + walkable + '">0</td>';
 				}
 		}
 		if(holeRight === true) {
-				res+='<td class="air">0</td>';
+				res+='<td class="' + backgroundC + '">0</td>';
 		}
 		return res += '</tr>';
 }
 
-function generateWalkable(stair, holeRight) {
+function generateWalkable(stairs, holeRight) {
 		var res = '<tr>';
 		var max = width;
 		var i = 0;
 		if(holeRight === false) {
-				res+='<td class="air">0</td>';
+				res+='<td class="' + backgroundC + '">0</td>';
 				i++;
 		} else if(holeRight === true) {
 				max--;
 		}
 		for(i; i < max; i++) {
-				if(!stair.includes(i)) {
-					res+='<td class="walkable">0</td>';
+				if(!stairs.includes(i)) {
+					res+='<td class="'+ backgroundC + ' ' + walkable + '">0</td>';
 				} else {
-					res+='<td class="stair">0</td>';
+					res+='<td class="' + stairC + ' ' + walkable + '">0</td>';
 				}
 		}
 		if(holeRight === true) {
-				res+='<td class="air">0</td>';
+				res+='<td class="'+ backgroundC + '">0</td>';
 		}
 		return res += '</tr>';
 }
 
-function generateAir(stair) {
-		var res = '<tr>'
+function generateAir(stairs) {
+		var res = '<tr>';
 		var max = width;
 		var i = 0;
 		for(i; i < max; i++) {
-				if(!stair.includes(i)) {
-					res+='<td class="air">0</td>';
+				if(!stairs.includes(i)) {
+					res+='<td class="'+ backgroundC + '">0</td>';
 				} else {
-					res+='<td class="stair">0</td>';
+					res+='<td class="' + stairC + ' ' + walkable + '">0</td>';
 				}
 		}
 		return res += '</tr>';
@@ -84,18 +92,75 @@ function generateMap() {
 		}
 		$("table").append(generateWalkable([], holeRight));
 		for(var i = 0; i < floor - 1; i++) {
-				var stair = generateRandomPositionStair();
-				$("table").append(generateFloor(stair, holeRight));
+				var stairs = generateRandomPositionStair();
+				$("table").append(generateFloor(stairs, holeRight));
 				for(var j = 0; j < height - 1; j++) {
-						$("table").append(generateAir(stair));
+						$("table").append(generateAir(stairs));
 				}
 				holeRight = !holeRight;
-				$("table").append(generateWalkable(stair, holeRight));
+				$("table").append(generateWalkable(stairs, holeRight));
 		}
 		$("table").append(generateFloor([]));
+}
+
+var eventRecorded;
+var playerPos;
+
+function resolveInputs() {
+	console.log(eventRecorded);
+	switch(eventRecorded) {
+		/*RIGHT*/
+		case 39:
+			if($(playerPos).next("td").hasClass(walkable) && $(playerPos).next("td")) {
+				$(playerPos).toggleClass(playerC);
+				playerPos = $(playerPos).next("td");
+				$(playerPos).toggleClass(playerC);
+			}
+		break;
+		/*LEFT*/
+		case 37:
+			if($(playerPos).prev("td").hasClass(walkable) && $(playerPos).prev("td")) {
+				$(playerPos).toggleClass(playerC);
+				playerPos = $(playerPos).prev("td");
+				$(playerPos).toggleClass(playerC);
+			}
+		break;
+		/*UP*/
+		case 38:
+			var nextPos = $(playerPos).parent("tr").prev("tr").children('td:nth-child(' + ($(playerPos).prevAll("td").length + 1) + ')');
+			if($(nextPos).hasClass(walkable)) {
+				$(playerPos).toggleClass(playerC);
+				playerPos = nextPos;
+				$(playerPos).toggleClass(playerC);
+			}
+		break;
+		/*DOWN*/
+		case 40:
+			var nextPos = $(playerPos).parent("tr").next("tr").children('td:nth-child(' + ($(playerPos).prevAll("td").length + 1) + ')');
+			if($(nextPos).hasClass(walkable)) {
+				$(playerPos).toggleClass(playerC);
+				playerPos = nextPos;
+				$(playerPos).toggleClass(playerC);
+			}
+		break;
+	}
+	eventRecorded = undefined;
 }
 
 $(document).ready(function() {
 		$("div.matchResult").hide();
 		generateMap();
+
+		playerPos = $("table tr:last-child").prev().children("td:first-child");
+		$(playerPos).addClass(backgroundC);
+		$(playerPos).addClass(walkable);
+		$(playerPos).addClass(playerC);
+
+		document.addEventListener('keydown', function(event){
+			eventRecorded = event.keyCode;
+		});
+
+		setInterval(function(){
+			resolveInputs();
+		}, 100);
 });
